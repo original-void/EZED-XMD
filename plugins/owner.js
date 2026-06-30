957e89cc0eb42b54650bd4b8075a20b6ea601964
original-void <void321m@gmail.com>
2026-06-30 11:41:13 +0300

    Refactor execute function in owner.js
    Updated the execute function to remove config parameter and modified the message format.

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,16 +1,22 @@
+const config = require("../config");
+
 module.exports = {
+
     name: "owner",
 
-    async execute({ sock, from, config }) {
+    async execute({ sock, from }) {
 
         await sock.sendMessage(from, {
-            text: `👑 *BOT OWNER*
+
+            text:
+`👑 BOT OWNER
 
 Name : ${config.OWNER_NAME}
-Number : ${config.OWNER_NUMBER}
 
-Powered by EZED X TECH`
+Phone : ${config.OWNER_NUMBER}`
+
         });
 
     }
+
 };
